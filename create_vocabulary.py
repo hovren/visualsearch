15e2839cb4b7a1e7c3982a99d94a05f81a31ca2c
Hannes Ovrén <hannes.ovren@liu.se>
2016-12-06 17:54:05 +0100

    Use scipy clustering for vocabulary

--- create_vocabulary.py
+++ create_vocabulary.py
@@ -8,6 +8,7 @@ import time
 import cv2
 import h5py
 import numpy as np
+import scipy.cluster
 
 import tqdm
 
@@ -17,17 +18,21 @@ def load_SIFT_descriptors(path):
         
 def save_vocabulary(means, path):
     with h5py.File(path, 'w') as f:
-        f['vocabulary'] = means  
+        f['vocabulary'] = means
+        
+def kmeans_score(data, centroids, labels):
+    return sum(np.linalg.norm(x - centroids[l])**2 for x, l in zip(data, labels))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('directory')
     parser.add_argument('out')
     parser.add_argument('size', type=int)
+    parser.add_argument('--kmeans', choices=['opencv', 'scipy'], default='scipy')
     args = parser.parse_args()
     
     out_path = os.path.expanduser(args.out)
-    sift_files = glob.glob(os.path.join(os.path.expanduser(args.directory), '*.sift.h5'))[:2]
+    sift_files = glob.glob(os.path.join(os.path.expanduser(args.directory), '*.sift.h5'))
 
     sift_descriptors = []
     for path in tqdm.tqdm(sift_files):
@@ -37,19 +42,33 @@ if __name__ == "__main__":
     data = np.vstack(sift_descriptors)
     print('Loaded {} SIFT descriptors from {} files'.format(len(data), len(sift_descriptors)))
     
-    max_iter = 5
-    eps = 1.0 # Not used
-    termcrit = (cv2.TERM_CRITERIA_MAX_ITER, max_iter, eps)
+    iterations = 5
     attempts = 10
     clusters = args.size
-    print('Clustering vocabulary with K={}, {:d} iterations and {:d} attempts'.format(args.size, max_iter, attempts))
     
+    
+    print('Clustering vocabulary using {} with K={}, {:d} iterations and {:d} attempts'.format(args.kmeans, args.size, iterations, attempts))
+
     t0 = time.time()
-    score, labels, means = cv2.kmeans(data, clusters, None, termcrit, attempts, cv2.KMEANS_RANDOM_CENTERS)
+    if args.kmeans == 'opencv':
+        eps = 1.0 # Not used
+        termcrit = (cv2.TERM_CRITERIA_MAX_ITER, iterations, eps)
+        score, labels, centroids = cv2.kmeans(data, clusters, None, termcrit, attempts, cv2.KMEANS_RANDOM_CENTERS)
+        print('Scipy score:', kmeans_score(data, centroids, labels))
+    elif args.kmeans == 'scipy':
+        score = np.inf
+        centroids = None
+        for i in tqdm.tqdm(range(attempts)):
+            iter_centroids, labels = scipy.cluster.vq.kmeans2(data, clusters, iterations, minit='points')
+            iter_score = kmeans_score(data, iter_centroids, labels)
+            if iter_score < score:
+                centroids = iter_centroids
+                score = iter_score
+                
     elapsed = time.time() - t0
     
     print('Clustering took {:.1f} seconds'.format(elapsed))
-    print('K-means compactness score {:.2f}'.format(score))
+    print('K-means compactness score {:g}'.format(score))
     out_file = os.path.expanduser(args.out)
-    save_vocabulary(means, out_file)
+    save_vocabulary(centroids, out_file)
     print('Saved vocabulary to', out_file)
