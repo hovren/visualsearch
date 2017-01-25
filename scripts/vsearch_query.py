d1ba42194e7d61dc0174ddd375980ead5105dd77
Hannes Ovrén <hannes.ovren@liu.se>
2017-01-25 17:12:39 +0100

    Load and save geofile

--- scripts/vsearch_query.py
+++ scripts/vsearch_query.py
@@ -27,10 +27,12 @@ class GuiWrappedDatabase(DatabaseWithLocation):
         self._marker_id_to_key = {}
         self._key_to_marker_id = {}
 
-        self.load_from_geofile()
+        self.load_geofile()
 
-    def load_from_geofile(self):
+    def load_geofile(self):
+        print('Loading geo locations from', self.geofile_path)
         if not (self.geofile_path and os.path.exists(self.geofile_path)):
+            print('Error in geofile path')
             return
 
         with open(self.geofile_path, 'r') as f:
@@ -41,8 +43,17 @@ class GuiWrappedDatabase(DatabaseWithLocation):
                     pass
                 key = key.strip()
                 latlng = LatLng(float(lat), float(lng))
-                old = self[key]
-                new = DatabaseEntry(old.key, old.bow, latlng)
+                print(key, latlng)
+                self.update_location(key, latlng)
+
+    def save_geofile(self):
+        if not self.geofile_path:
+            raise ValueError("No geofile path set")
+
+        with open(self.geofile_path, 'w') as f:
+            for entry in self.values():
+                if entry.latlng:
+                    f.write('{:s}, {:f}, {:f}\n'.format(entry.key, entry.latlng.lat, entry.latlng.lng))
 
     def remove_all_markers(self):
         self.map_widget.remove_all_markers()
@@ -111,13 +122,13 @@ class MainWindow(w.QMainWindow):
         def load_db():
             dialog = LoadDatabaseDialog(self)
             if dialog.exec_():
-                self.load_database(dialog.db_path, dialog.image_root, dialog.geofile)
+                self.load_database(dialog.db_path, dialog.image_root, geofile_path=None)
 
 
         load_database_button.clicked.connect(load_db)
 
-        clear_markers_button = w.QPushButton("Clear markers")
-        clear_markers_button.clicked.connect(lambda *args: self.map_view.clear_markers())
+        save_geo_button = w.QPushButton("Save Locations")
+        save_geo_button.clicked.connect(self.on_save_locations)
 
         # Layout
         vbox1 = w.QVBoxLayout()
@@ -135,6 +146,7 @@ class MainWindow(w.QMainWindow):
         hbox.addWidget(tab_widget)
 
         button_hbox.addWidget(load_database_button)
+        button_hbox.addWidget(save_geo_button)
 
         vbox1.addLayout(button_hbox)
         vbox1.addLayout(hbox)
@@ -143,6 +155,9 @@ class MainWindow(w.QMainWindow):
         dummy.setLayout(vbox1)
 
         self.setCentralWidget(dummy)
+
+    def on_save_locations(self):
+        self.database.save_geofile()
 
     def marker_clicked(self, marker_id):
         print('clicked marker #{:d}'.format(marker_id))
@@ -165,8 +180,19 @@ class MainWindow(w.QMainWindow):
         self.database_page.image_list.setCurrentItem(None)
 
 
-    def load_database(self, path, image_root, geofile_path):
+    def load_database(self, path, image_root, geofile_path=None):
         visual_database = AnnDatabase.from_file(path)
+
+        for key in visual_database.image_vectors:
+            path = os.path.join(image_root, key)
+            if not os.path.exists(path):
+                msg = "Database file/key '{}' not found in image directory '{}'. Please try again.".format(key, image_root)
+                w.QErrorMessage(self).showMessage(msg)
+                return
+
+        if not geofile_path:
+            geofile_path = os.path.join(image_root, 'geo.csv')
+
         self.database = GuiWrappedDatabase(visual_database, self.map_view, image_root, geofile_path)
         self.database_page.load_database(self.database)
         self.tab_widget.setCurrentIndex(DATABASE_TAB)
@@ -192,12 +218,12 @@ class DatabasePage(w.QWidget):
         clear_button = w.QPushButton("Clear selection")
         clear_button.clicked.connect(lambda: self.image_list.setCurrentItem(None))
 
-        save_location_button = w.QPushButton("Save location")
-        save_location_button.clicked.connect(self.save_location_clicked)
+        clear_location_button = w.QPushButton("Clear location")
+        clear_location_button.clicked.connect(self.clear_location_clicked)
 
         button_box = w.QHBoxLayout()
         button_box.addWidget(clear_button)
-        button_box.addWidget(save_location_button)
+        button_box.addWidget(clear_location_button)
 
 
         vbox = w.QVBoxLayout()
@@ -213,13 +239,16 @@ class DatabasePage(w.QWidget):
         self.database.remove_all_markers()
         self.database.add_all_markers()
 
-    def save_location_clicked(self):
+    def clear_location_clicked(self):
         item = self.image_list.currentItem()
         if item:
             key = item.text()
-            marker = self.database.marker_for_key(key)
-            latlng = LatLng(*marker.latlng)
-            self.database.update_location(key, latlng)
+            try:
+                marker = self.database.marker_for_key(key)
+                self.database.update_location(key, None)
+                marker.remove()
+            except KeyError:
+                pass # No marker
 
     def on_map_click(self, lat, lng):
         # Update only if an item is selected and doesn't already have a marker on the map
@@ -257,7 +286,7 @@ class DatabasePage(w.QWidget):
         elif self.database is not None:
             key = current.text()
             entry = self.database[key]
-            path = os.path.join(self.database.image_root, entry.key + '.jpg')
+            path = os.path.join(self.database.image_root, entry.key)
             image = cv2.imread(path)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             self.image.set_array(image)
@@ -320,8 +349,8 @@ class LoadDatabaseDialog(w.QDialog):
         self._db_path.setMinimumWidth(width)
         self._image_root = LineFileChooser(directory=True)
         self._db_path.setMinimumWidth(width)
-        self._geofile = LineFileChooser(filter='*.csv')
-        self._geofile.setMinimumWidth(width)
+        #self._geofile = LineFileChooser(filter='*.csv')
+        #self._geofile.setMinimumWidth(width)
 
         bb = w.QDialogButtonBox(w.QDialogButtonBox.Ok | w.QDialogButtonBox.Cancel)
         bb.accepted.connect(self.accept)
@@ -330,7 +359,7 @@ class LoadDatabaseDialog(w.QDialog):
         form = w.QFormLayout()
         form.addRow("Visual database", self._db_path)
         form.addRow("Image root", self._image_root)
-        form.addRow("Location file", self._geofile)
+        #form.addRow("Location file", self._geofile)
         form.addWidget(bb)
 
         self.setLayout(form)
@@ -343,12 +372,22 @@ class LoadDatabaseDialog(w.QDialog):
     def image_root(self):
         return self._image_root.line.text()
 
-    @property
-    def geofile(self):
-        return None
+    #@property
+    #def geofile(self):
+    #    return None
 
 
 if __name__ == "__main__":
     app = w.QApplication(sys.argv)
     mwin = MainWindow()
+
+    def on_load():
+        mwin.load_database('/home/hannes/Projects/VS-imsearch/test/test_db.h5', '/home/hannes/Datasets/narrative2')
+
+    from PyQt5.QtCore import QTimer
+    timer = QTimer(mwin)
+    timer.setSingleShot(True)
+    timer.timeout.connect(on_load)
+    timer.start(1000)
+
     sys.exit(app.exec_())
